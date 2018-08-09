e67cdc37fe3e30e30ebb03306ebff911d67db9cc
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-08-09 16:13:05 +0200

    transition times of hovering over links reduced, in line with users' feedback (was too slow for comfort earlier)

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
     /* work link hover effect */
 
     $("#link_work_shape").hover(function() {
-        $("#link_work_shape").css({fill: "#e7e7e7", transition: "3s"});
-        $("#link_work_shape").css({stroke: "#084E96", transition: "3s"});
+        $("#link_work_shape").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#link_work_shape").css({stroke: "#084E96", transition: "0.5s"});
         if($(window).width() < 640) {
             $("#link_work_shape").css({strokeWidth: "1.6px", transition: "0s"});
         } else {
             $("#link_work_shape").css({strokeWidth: "0.8px", transition: "0s"});
         }
-        $("#nav_work_text").css({fill: "#084E96", transition: "3s"});
-        $("#whiteline_work").hide("1s");
+        $("#nav_work_text").css({fill: "#084E96", transition: "0.5s"});
+        $("#whiteline_work").hide("0.5s");
     }, function() {
     });
 
@@ -22,15 +22,15 @@ $(document).ready(function() {
     /* contact link hover effect */
 
     $("#link_contact_shape").hover(function() {
-        $("#link_contact_shape").css({fill: "#e7e7e7", transition: "3s"});
-        $("#link_contact_shape").css({stroke: "#9b0408", transition: "3s"});
+        $("#link_contact_shape").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#link_contact_shape").css({stroke: "#9b0408", transition: "0.5s"});
         if($(window).width() < 640) {
             $("#link_contact_shape").css({strokeWidth: "1.6px", transition: "0s"});
         } else {
             $("#link_contact_shape").css({strokeWidth: "0.8px", transition: "0s"});
         }
-        $("#nav_contact_text").css({fill: "#9b0408", transition: "3s"});
-        $("#whiteline_contact").hide("1s");
+        $("#nav_contact_text").css({fill: "#9b0408", transition: "0.5s"});
+        $("#whiteline_contact").hide("0.5s");
     }, function() {
     });
 
@@ -38,10 +38,10 @@ $(document).ready(function() {
     /* about link 1 hover effect */
 
     $("#link_about1_shape").hover(function() {
-        $("#link_about1_shape").css({fill: "#084E96", transition: "3s"});
-        $("#link_about1_shape").css({stroke: "#e7e7e7", transition: "3s"});
-        $("#link_about1_shape").css({strokeWidth: "2.6px", transition: "3s"});
-        $("#nav_about1_text").css({fill: "#e7e7e7", transition: "3s"});
+        $("#link_about1_shape").css({fill: "#084E96", transition: "0.5s"});
+        $("#link_about1_shape").css({stroke: "#e7e7e7", transition: "0.5s"});
+        $("#link_about1_shape").css({strokeWidth: "2.6px", transition: "0.5s"});
+        $("#nav_about1_text").css({fill: "#e7e7e7", transition: "0.5s"});
     }, function() {}
     );
 
@@ -49,10 +49,10 @@ $(document).ready(function() {
     /* about link 2 hover effect */
 
     $("#link_about2_shape").hover(function() {
-        $("#link_about2_shape").css({fill: "#084E96", transition: "3s"});
-        $("#link_about2_shape").css({stroke: "#e7e7e7", transition: "3s"});
-        $("#link_about2_shape").css({strokeWidth: "0px", transition: "3s"});
-        $("#nav_about2_text").css({fill: "#e7e7e7", transition: "3s"});
+        $("#link_about2_shape").css({fill: "#084E96", transition: "0.5s"});
+        $("#link_about2_shape").css({stroke: "#e7e7e7", transition: "0.5s"});
+        $("#link_about2_shape").css({strokeWidth: "0px", transition: "0.5s"});
+        $("#nav_about2_text").css({fill: "#e7e7e7", transition: "0.5s"});
     }, function() {
     });
 
@@ -60,15 +60,15 @@ $(document).ready(function() {
     /* send link hover effect */
 
     $("#link_send_shape").hover(function() {
-        $("#link_send_shape").css({fill: "#9b0408", transition: "3s"});
-        $("#link_send_shape").css({stroke: "#e7e7e7", transition: "3s"});
+        $("#link_send_shape").css({fill: "#9b0408", transition: "0.5s"});
+        $("#link_send_shape").css({stroke: "#e7e7e7", transition: "0.5s"});
         if($(window).width() < 640) {
             $("#link_send_shape").css({strokeWidth: "1.6px", transition: "0s"});
         } else {
             $("#link_send_shape").css({strokeWidth: "0.8px", transition: "0s"});
         }
-        $("#button_send_text").css({fill: "#e7e7e7", transition: "3s"});
-        $("#redline_send").hide("1s");
+        $("#button_send_text").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#redline_send").hide("0.5s");
     }, function() {
     });
 
@@ -78,40 +78,41 @@ $(document).ready(function() {
 
         /* work link reset */
 
-        $("#link_work_shape").css({fill: "#084E96", transition: "3s"});
-        $("#link_work_shape").css({stroke: "#084E96", transition: "3s"});
-        $("#link_work_shape").css({strokeWidth: "0px", transition: "0s"});
-        $("#nav_work_text").css({fill: "#e7e7e7", transition: "3s"});
+        console.log("this is happening!!!");   /* probeer op de css de transition te zetten!!! */
+        $("#link_work_shape").css({fill: "#084E96", transition: "0.5s"});
+        $("#link_work_shape").css({stroke: "#084E96", transition: "0.5s"});
+        $("#link_work_shape").css({strokeWidth: "0px", transition: "0.5s"});
+        $("#nav_work_text").css({fill: "#e7e7e7", transition: "0.5s"});
         $("#whiteline_work").show("1s");
 
         /* contact link reset */
 
-        $("#link_contact_shape").css({fill: "#9b0408", transition: "3s"});
-        $("#link_contact_shape").css({stroke: "#9b0408", transition: "3s"});
-        $("#link_contact_shape").css({strokeWidth: "0px", transition: "0s"});
-        $("#nav_contact_text").css({fill: "#e7e7e7", transition: "3s"});
+        $("#link_contact_shape").css({fill: "#9b0408", transition: "0.5s"});
+        $("#link_contact_shape").css({stroke: "#9b0408", transition: "0.5s"});
+        $("#link_contact_shape").css({strokeWidth: "0px", transition: "0.5s"});
+        $("#nav_contact_text").css({fill: "#e7e7e7", transition: "0.5s"});
         $("#whiteline_contact").show("1s");
 
         /* about link 1 reset */
 
-        $("#link_about1_shape").css({fill: "#e7e7e7", transition: "3s"});
-        $("#link_about1_shape").css({stroke: "#084E96", transition: "3s"});
-        $("#link_about1_shape").css({strokeWidth: "2.6px", transition: "3s"});
-        $("#nav_about1_text").css({fill: "#084E96", transition: "3s"});
+        $("#link_about1_shape").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#link_about1_shape").css({stroke: "#084E96", transition: "0.5s"});
+        $("#link_about1_shape").css({strokeWidth: "2.6px", transition: "0.5s"});
+        $("#nav_about1_text").css({fill: "#084E96", transition: "0.5s"});
 
         /* about link 2 reset */
 
-        $("#link_about2_shape").css({fill: "#e7e7e7", transition: "3s"});
-        $("#link_about2_shape").css({stroke: "#084E96", transition: "3s"});
-        $("#link_about2_shape").css({strokeWidth: "1.2px", transition: "3s"});
-        $("#nav_about2_text").css({fill: "#084E96", transition: "3s"});
+        $("#link_about2_shape").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#link_about2_shape").css({stroke: "#084E96", transition: "0.5s"});
+        $("#link_about2_shape").css({strokeWidth: "1.2px", transition: "0.5s"});
+        $("#nav_about2_text").css({fill: "#084E96", transition: "0.5s"});
 
         /* send button reset */
 
-        $("#link_send_shape").css({fill: "#e7e7e7", transition: "3s"});
-        $("#link_send_shape").css({stroke: "#e7e7e7", transition: "3s"});
-        $("#link_send_shape").css({strokeWidth: "0px", transition: "0s"});
-        $("#button_send_text").css({fill: "#9b0408", transition: "3s"});
+        $("#link_send_shape").css({fill: "#e7e7e7", transition: "0.5s"});
+        $("#link_send_shape").css({stroke: "#e7e7e7", transition: "0.5s"});
+        $("#link_send_shape").css({strokeWidth: "0px", transition: "0.5s"});
+        $("#button_send_text").css({fill: "#9b0408", transition: "0.5s"});
         $("#redline_send").show("1s");
 
     }, function() {
